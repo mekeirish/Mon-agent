f0e76e0d8326a960e01b79a17ab36ca24d199830
mekeirish <mmm1747lb@gmail.com>
2026-05-16 00:01:13 +0200

    Mastercode Sync

--- sw.js
+++ sw.js
@@ -1,18 +1,1 @@
-const CACHE_NAME = 'agent-ia-v1';
-const ASSETS = [
-  'index.html',
-  'manifest.json',
-  'icon.png'
-];
-
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
-});
-
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
-  );
-});
+const cacheName='Braincode-v1'; self.addEventListener('install',e=>{e.waitUntil(caches.open(cacheName).then(c=>c.addAll(['./index.html','./manifest.json','./icon.png'])));}); self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
